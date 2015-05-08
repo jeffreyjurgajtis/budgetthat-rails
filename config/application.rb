f4d26c737aae05c4ccc21620a93555365be67ecf
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-05-08 11:20:18 -0500

    Silence request logger

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module PearApi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins('http://localhost:4200')
 
